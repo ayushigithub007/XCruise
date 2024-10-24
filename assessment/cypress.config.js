16acb6456eec48b0e709eff7d6b71f5a81f559a3
Mohak-Trivedi <ayushisharmafzd1@gmail.com>
2024-10-24 12:31:19 +0530

    corrected the wrong updates made by DSA interns  (Crio.Do)

--- assessment/cypress.config.js
+++ assessment/cypress.config.js
@@ -1,4 +1,3 @@
-
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
@@ -6,18 +5,5 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
-  reporter: "mochawesome",
-  reporterOptions: {
-    // To display small circular charts regarding test results
-    charts: true,
-    // Generate JSON file to create custom reports
-    json: true,
-    // Customize the directory in which reports are saved
-    reportsDir: "reports/your-reports-folder",
-    // Customize the report file name
-    reportFilename: "my-report",
-    // Generate new report file or overwrite the a single file
-    overwrite: false,
   },
 });
