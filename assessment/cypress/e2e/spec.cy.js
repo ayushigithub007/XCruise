88735de4548bace11595615c0640c6e986671d07
ayushigithub007 <ayushisharmafzd1@gmail.com>
2025-10-15 18:19:15 +0530

    “ME_FE_BUILDOUT_XCRUISE_MODULE_ONE”  (Crio.Do)

--- assessment/cypress/e2e/spec.cy.js
+++ assessment/cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('xCruise Landing Page Tests', () => {
   beforeEach(() => {
     // Visit the xCruise home page before each test
-    cy.visit('http://13.233.252.91:8081/') // Replace with the actual URL
+    cy.visit('http://13.233.130.0:8081/') // Replace with the actual URL
   });
 
   it('Navigation Bar - should display the xCruise logo', () => {
